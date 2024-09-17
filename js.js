2727f5fcf54b73e5d673426932fc2c9c01a45d34
Духлинцева Юлия <duhlincevayulia791@gmail.com>
2024-09-17 07:10:50 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,22 @@
+let hour = 24
+let minut = 60
+let sec = 60
+let days = 30
+let year = 365
+console.log(`в сутках ${hour*minut*sec} секунд`)
+console.log(`в 30 сутках ${hour*minut*sec*days} секунд`)
+console.log(`в году ${hour*minut*sec*year} секунд`)
+console.log(`в сутках ${hour*minut} минут`)
+console.log(`в году ${hour*minut*year} минут`)
+let bait = 1
+let kb = 1024
+let mb = 1024
+let gb = 1024
+let tengb = 10240
+let tb = 1024
+console.log(`В мегабайте ${bait*kb*mb} байт`)
+console.log(`В гигабайте ${bait*kb*mb*gb} байт`)
+console.log(`В 10 гигабайтах ${bait*kb*mb*tengb} байт`)
+console.log(`В гигабайте ${bait*kb*mb*gb} байт`)
+console.log(`В терабайте ${bait*kb*mb*gb*tb} байт`)
+console.log(`В терабайте ${kb*mb*gb*tb} килобайт`)
